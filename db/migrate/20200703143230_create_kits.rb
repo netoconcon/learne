63e5c446e475fc805db635b58f9c872c8a809e0b
rafael osawa <osawa.rafael@gmail.com>
2020-07-03 17:32:56 +0300

    added model kit

---
+++ db/migrate/20200703143230_create_kits.rb
@@ -0,0 +1,20 @@
+class CreateKits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :kits do |t|
+      t.string :name
+      t.string :description
+      t.string :payment_type
+      t.integer :standard_installments
+      t.integer :maximum_installments
+      t.integer :shipment_cost
+      t.string :shipment_description
+      t.boolean :allow_free_shipment
+      t.integer :weight
+      t.integer :width
+      t.integer :height
+      t.integer :length
+
+      t.timestamps
+    end
+  end
+end
