0eea5cc63952d2d3c3d88ba9b90885178c8654e6
rafael osawa <osawa.rafael@gmail.com>
2020-07-06 17:20:02 +0300

    fixing migration

--- db/migrate/20200706140600_change_payment_type_in_kits.rb
+++ db/migrate/20200706140600_change_payment_type_in_kits.rb
@@ -1,5 +1,5 @@
 class ChangePaymentTypeInKits < ActiveRecord::Migration[6.0]
   def change
-    change_column :kits, :payment_type, :intege
+    change_column :kits, :payment_type, 'integer USING CAST(payment_type AS integer)'
   end
 end
