3df262eb0c74d76f6c1f9b03d6825024a3705695
Neto Concon <concon.neto2@gmail.com>
2020-07-06 10:08:39 -0300

    remove admin table

--- db/migrate/20200703142301_create_companies.rb
+++ db/migrate/20200703142301_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
       t.boolean :international
-      t.string :CNPJ
+      t.string :cnpj
       t.string :email_notification
       t.string :email_support
       t.string :phone_support
