5adb993b3ef519b05b875e688ea1f17e7f1bf3a0
viniciusfestalab <vinicius@festalab.com>
2020-07-07 10:04:14 -0300

    added suffix for payment_type enum

--- app/models/kit.rb
+++ app/models/kit.rb
@@ -2,5 +2,5 @@ class Kit < ApplicationRecord
   has_many :kit_products
   has_many :products, through: :kit_products
 
-  enum payment_type: { single: 0, subscription: 1 }
+  enum payment_type: { single: 0, subscription: 1 }, _suffix: :payment
 end
