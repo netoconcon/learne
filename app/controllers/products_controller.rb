a2964fefc9b6d3bb9f7fedf835e04cc64be82ba3
rafael osawa <osawa.rafael@gmail.com>
2020-07-08 22:10:35 +0300

    Cadastrar produto

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
   def index
-    
+    @products = Product.all
   end
 
   def new
@@ -9,17 +9,32 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.company_id = params[:company_id]
+    @company = params[:company_id]
     if @product.save!
-      redirect_to companies_path
+      redirect_to products_path
     else
       render :new
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    if @product.save!
+      redirect_to products_path
+    else
+      render :new
+    end
+  end
+
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :sku, :price, :description, :external_id, :weight, :height, :length, :virtual_url)   
+    params.require(:product).permit(:company_id, :name, :sku, :price, :description, :external_id, :weight, :height, :length, :virtual_url)   
   end
 end
