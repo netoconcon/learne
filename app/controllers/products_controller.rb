49260a81614710dea0d5abc0d1d9893972b76858
rafael osawa <osawa.rafael@gmail.com>
2020-07-08 22:59:35 +0300

    Fix products fields

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -31,6 +31,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
 
   private
 
