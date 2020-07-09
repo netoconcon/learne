51ff521c5a919e41854c75fac36a16722b5ede52
rafael osawa <osawa.rafael@gmail.com>
2020-07-09 19:59:28 +0300

    Tried to make a dynamic form with JS but didnt work

---
+++ app/javascript/form/form.js
@@ -0,0 +1,7 @@
+// var el = document.getElementById('infoproduto')
+// if(el){
+//   el.addEventListener('change', function() {
+//   var style = this.value == false ? 'block' : 'none';
+//   document.getElementById('physical').style.display = style;
+// });
+// }
