a7f8d6622171dace586fdd7849c19d25d62c32cd
rafael osawa <osawa.rafael@gmail.com>
2020-07-10 15:11:17 +0300

    selling pages done

---
+++ app/controllers/selling_pages_controller.rb
@@ -0,0 +1,46 @@
+class SellingPagesController < ApplicationController
+  def index
+    @selling_pages = SellingPage.all
+  end
+
+  def new
+    @selling_page = SellingPage.new
+  end
+
+  def create
+    @selling_page = SellingPage.new(selling_page_params)
+    @company = params[:company_id]
+    if @selling_page.save!
+      redirect_to selling_pages_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @selling_page = SellingPage.find(params[:id])
+  end
+
+  def update
+    @selling_page = SellingPage.find(params[:id])
+    @selling_page.update(selling_page_params)
+    if @selling_page.save!
+      redirect_to selling_pages_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @selling_page = SellingPage.find(params[:id])
+    @selling_page.destroy
+    redirect_to selling_pages_path
+  end
+
+
+  private
+
+  def selling_page_params
+    params.require(:selling_page).permit(:product_id, :name, :description, :url)   
+  end
+end
--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -3,6 +3,7 @@ resources :financials, only: [:index]
 resources :settings, only: [:index]
 resources :products
 resources :kits
+resources :selling_pages
 resources :companies do
   resources :bank_accounts
 end
