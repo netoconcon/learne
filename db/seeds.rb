f0ba91f07ffc94b87d42641f211eddf7af804330
rafael osawa <osawa.rafael@gmail.com>
2020-07-14 05:31:04 +0300

    Add back buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+company.destroy
+
+puts 'Criando companhias'
+10.times do
+  company = Company.new(
+    name: Faker::Company.name,
+    cnpj: Faker::Company.brazilian_company_number(formatted: true),
+    email = Faker::Internet.email,
+    email_support = email,
+    email_notification = email
+
+  )
+  company.save!
+end
