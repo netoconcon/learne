e35ae56c3807232094a34f9903e6dd6b47cdb389
rafael osawa <osawa.rafael@gmail.com>
2020-07-17 02:45:45 +0300

    cocoon implemented but need some fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,7 +22,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
-
+import 'cocoon';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import 'controllers'
--- app/models/kit.rb
+++ app/models/kit.rb
@@ -1,6 +1,8 @@
 class Kit < ApplicationRecord
   has_many :kit_products, dependent: :destroy
   has_many :products, through: :kit_products
+  accepts_nested_attributes_for :kit_products, reject_if: :all_blank, allow_destroy: :true
+
 
   enum payment_type: { single: 0, subscription: 1 }, _suffix: :payment
 end
