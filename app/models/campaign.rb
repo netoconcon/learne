e9993bf46bd2137e3c3df2fe084e34fe62329415
rafael osawa <osawa.rafael@gmail.com>
2020-07-23 00:35:30 +0300

    Campaigns validations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,7 @@
 class Campaign < ApplicationRecord
+
+  validates :title, presence: true
+  
+
   belongs_to :selling_page
 end
--- app/models/selling_page.rb
+++ app/models/selling_page.rb
@@ -1,4 +1,8 @@
 class SellingPage < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :url, presence: true
+
   belongs_to :product
   has_many :campaigns, dependent: :destroy
 end
