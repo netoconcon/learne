ef69d4f2fb3ab6bfa20321118913b8bbacaa264d
rafael osawa <osawa.rafael@gmail.com>
2020-07-23 18:21:13 +0300

    orders validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,19 @@
 class Order < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :zipcode, presence: true
+  validates :street, presence: true
+  validates :street_number, presence: true
+  validates :neighborhood, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :complement, presence: true
+  validates :price, presence: true
+  validates :CPF, presence: true
+  validates :birthday, presence: true
+
+
   belongs_to :kit
 end
