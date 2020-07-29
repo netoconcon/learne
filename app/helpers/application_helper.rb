5bb276d0564779daee22e69297b1561c374d0ce7
rafael osawa <osawa.rafael@gmail.com>
2020-07-30 00:14:37 +0300

    modal on kits

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,3 @@
 module ApplicationHelper
 
-  def kit_product_index
-    show_products = []
-    kit_products = KitProduct.all
-    @kits.each do |kit|
-      kit_products.each do |kit_product|
-        if kit.id == kit_product.id
-          show_products << kit_product
-        end
-      end
-    end
-    return show_products
-  end
 end
