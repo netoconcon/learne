3a224b3f6dabba013498be21c5d6b55588c307dd
rafael osawa <osawa.rafael@gmail.com>
2020-08-05 18:47:56 +0300

    infoproduct fields displaying and hiding

--- app/javascript/controllers/infoproduct_controller.js
+++ app/javascript/controllers/infoproduct_controller.js
@@ -1,9 +1,21 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [  ]
+  static targets = [ "specs", "vurl"]
 
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+  selection(e) {
+    const state = e.currentTarget.value;
+    console.log(state)
+		if (state == 'false') {
+			this.specsTarget.style.display = "block";
+			this.vurlTarget.style.display = "none";
+		} else if (state == 'true') {
+			this.vurlTarget.style.display = "block";
+			this.specsTarget.style.display = "none";
+		}
+		else {
+			this.vurlTarget.style.display = "none";
+			this.specsTarget.style.display = "none";
+		}
   }
 }
