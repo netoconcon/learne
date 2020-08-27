8d5154bc4403e379323714612b5126759956c9a5
viniciusfestalab <vinicius@festalab.com>
2020-08-27 14:02:44 -0300

    fixes

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,7 +2,7 @@ module OrdersHelper
   def order_installments(order)
     installments = []
     order.kit.maximum_installments.times do |index|
-      installments << ["#{index} X #{number_to_currency(order.kit.price, unit: "R$ ", separator: ",")}", index]
+      installments << ["#{index} X #{number_to_currency(order.kit.price, unit: "R$ ", separator: ",") / index}", index]
     end
     installments
   end
