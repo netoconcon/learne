6fa5c02c82762778e501165789c2949ffdc9fcf9
viniciusfestalab <vinicius@festalab.com>
2020-08-27 13:32:22 -0300

    fixed login

--- app/models/user/validation.rb
+++ app/models/user/validation.rb
@@ -2,7 +2,6 @@ module User::Validation
   extend ActiveSupport::Concern
 
   included do
-    validates :phone, format: Formatter.phone
     validates :email, presence: true, format: Formatter.email
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_151618) do
+ActiveRecord::Schema.define(version: 2020_08_27_163017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -166,8 +166,8 @@ ActiveRecord::Schema.define(version: 2020_08_27_151618) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
-    t.string "first_name", null: false
-    t.string "last_name", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.string "phone"
     t.string "cpf"
     t.string "birthday"
