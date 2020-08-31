33efdaf2e6133742728743e6cd7b2bfe916f2936
rafael osawa <osawa.rafael@gmail.com>
2020-08-31 18:07:27 +0300

    added campaigns to admin

---
+++ app/controllers/admin/campaigns_controller.rb
@@ -0,0 +1,45 @@
+class Admin::CampaignsController < ApplicationController
+  layout "admin"
+
+  def index
+    @campaigns = Campaign.all.order('created_at ASC')
+  end
+  def new
+    @campaign = Campaign.new
+  end
+
+  def create
+    @campaign = Campaign.new(campaign_params)
+    if @campaign.save
+      redirect_to campaigns_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @campaign = Campaign.find(params[:id])
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+    @campaign.update(campaign_params)
+    if @campaign.save
+      redirect_to campaigns_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @campaign = Campaign.find(params[:id])
+    @campaign.destroy
+    redirect_to campaigns_path
+  end
+
+  private
+
+  def campaign_params
+    params.require(:campaign).permit(:selling_page_id, :fbid, :utm_source, :utm_campaign, :utm_term,:utm_medium, :utm_medium, :utm_content, :pubid, :title)
+  end
+end
