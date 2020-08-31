dfc5ecbceeb1075b6bf492bf2f8290ff5dd12baf
rafael osawa <osawa.rafael@gmail.com>
2020-08-31 11:59:03 +0300

    cleaning branch

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,15 +1,4 @@
-<<<<<<< HEAD
-resources :products
-resources :kits
-resources :kit_products
-resources :orders
 
-resources :selling_pages
-resources :campaigns
-resources :companies do
-  resources :bank_accounts
-end
-=======
 resources :products
 resources :kits
 resources :kit_products
@@ -24,4 +13,3 @@ end
 namespace :admin do
   resources :orders, only: [:index, :show]
 end
->>>>>>> 8f15b9c17c1c7f07dad90ba5488d0fe81a8e0f53
