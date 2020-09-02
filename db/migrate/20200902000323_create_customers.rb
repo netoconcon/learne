899f7cbe570dae5d56e09987294b8749f934c24c
netoconcon <concon.neto2@gmail.com>
2020-09-01 21:18:28 -0300

    add customer model

---
+++ db/migrate/20200902000323_create_customers.rb
@@ -0,0 +1,14 @@
+class CreateCustomers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :customers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :pagarme_card
+      t.string :card_fingerprint
+
+      t.timestamps
+    end
+  end
+end
