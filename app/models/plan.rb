abeea06f02de771c3d32605e0e9fde27fbc07052
netoconcon <concon.neto2@gmail.com>
2020-09-09 16:39:54 -0300

    add logic to plan model

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,20 @@
 class Plan < ApplicationRecord
+
+  def set_pagarme_plan
+    # Create a Plan in Pagarme's DB
+    plan = PagarMe::Plan.new({
+      name: self.name,
+      days: self.days,
+      installments: self.installments,
+      amount: self.amount,
+      payment_methods: ['credit_card']
+    })
+
+    plan.create
+
+    self.pagarme_id = plan.id
+    self.save!
+
+    raise "There was an error creating the plan" unless plan.id
+  end
 end
