e81ff534bda752de00b0c983668c9d1e6bbd03c0
rafael osawa <osawa.rafael@gmail.com>
2020-10-04 15:56:04 +0300

    cleaning

---
+++ app/javascript/controllers/select2_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "stimulus"
+import $ from 'jquery';
+
+require("select2/dist/css/select2")
+
+import Select2 from "select2"
+
+
+export default class extends Controller {
+  static targets = [ "bank" ]
+	
+	connect() {
+    $(this.currentTarget).select2();
+  }
+}
