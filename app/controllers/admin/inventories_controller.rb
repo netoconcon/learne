acc8b259fed7eae545bcca565598f70cb3fe1d13
rafael osawa <osawa.rafael@gmail.com>
2020-11-09 13:22:36 +0200

    Created inventory new form

--- app/controllers/admin/inventories_controller.rb
+++ app/controllers/admin/inventories_controller.rb
@@ -2,13 +2,46 @@ class Admin::InventoriesController < ApplicationController
   layout "admin"
 
   def index
-    @invetories = Inventory.all
+    @inventories = Inventory.all
     # @products = Product.all.where(virtual: false)
+  end
+
+  def new
+    @inventory = Inventory.new
+  end
+
+  def create
+    @inventory = Inventory.new(inventory_params)
+    if @inventory.save
+      redirect_to admin_inventories_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @inventory = Inventory.find(params[:id])
+  end
+
+  def update
+    @inventory = Inventory.find(params[:id])
+    @inventory.update(inventory_params)
+    if @inventory.save
+      redirect_to admin_inventories_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @inventory = Inventory.find(params[:id])
+    @inventory.destroy
+    redirect_to admin_inventories_path
   end
 
   private
 
-  def product_params
+  def inventory_params
     params.require(:inventory).permit(:product_id, :quantity)
   end
 end
