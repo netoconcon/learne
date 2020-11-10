7416ff724a738e39da414764f44883e587ddafe6
rafael osawa <osawa.rafael@gmail.com>
2020-11-10 18:35:29 +0200

    Added inventory to products

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,14 +1,6 @@
 class Inventory < ApplicationRecord
+  has_paper_trail
+
   belongs_to :product
-
-  before_update :product_changed
-
-  private
-
-  def product_changed
-    if self.changed?
-      @updating = self.changes
-    end
-  end
 
 end
