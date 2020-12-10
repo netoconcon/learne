358e61bf4aa5a4daf0c2871a0806eb6f2bfce549
rafael osawa <osawa.rafael@gmail.com>
2020-12-10 09:59:21 +0200

    added fixes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
 
   validates :name, :company, :price_cents, presence: true
+  validates_numericality_of :price_cents, :greater_than_or_equal_to => 0
 
   belongs_to :company
   
