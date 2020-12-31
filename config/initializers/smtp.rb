8f9d860b4fb004dd73ce12389e48b7f749ecd3db
Neto Concon <concon.neto2@gmail.com>
2020-12-31 15:38:18 -0300

    update mailer sender

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
   :port => 587,
-  :domain => "fitlab.com.br",
+  :domain => "northern.com.br",
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
