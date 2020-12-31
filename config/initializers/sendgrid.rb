4d9609bcfd87aa2985cf10e408607a6d1b9c6a6f
Neto Concon <concon.neto2@gmail.com>
2020-12-31 14:54:28 -0300

    fixed api

---
+++ config/initializers/sendgrid.rb
@@ -0,0 +1,5 @@
+sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+# response = sg.client.mail._('send').post(request_body: mail.to_json)
+# puts response.status_code
+# puts response.body
+# puts response.headers
