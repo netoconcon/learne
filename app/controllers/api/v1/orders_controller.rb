c2dbc35d1b129cf0af6febc9135f09abf2fceca3
Neto <concon.neto2@gmail.com>
2021-01-18 17:56:38 -0300

    fixed postabk

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -12,9 +12,10 @@ class Api::V1::OrdersController < Api::V1::BaseController
       if params["payload"]
         ary = URI.decode_www_form(params["payload"])
         payload = Hash[ary]
+        pagarme_id = pagarme_transaction_id.to_i
 
         # dealing with order
-          order = Order.find(params[:id])
+          order = Order.find_by(pagarme_id)
           order.status = payload.status
           order.save
         #
