bb7f72e690ec18e873a9167ab1c6256e56237627
vmfrcosta <vmfrcosta@gmail.com>
2021-04-11 23:29:38 -0300

    another small fix to dashboard

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,7 +1,8 @@
 module DashboardsHelper
   def performance(current_value, reference_value)
+    return 0 if reference_value == 0 && current_value == 0
     return 100 if reference_value == 0
-    
+
     ((current_value.to_f / reference_value.to_f) - 1) * 100
   end
 
