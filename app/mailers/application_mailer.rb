9e081ca30a67ba1b5c33a2393f4b0ac7192e29e3
Neto <concon.neto2@gmail.com>
2021-04-13 15:22:50 -0300

    fixed

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'cliente@learne.zendesk.com'
+  #   default from: 'cliente@learne.zendesk.com'
+  default from: 'neto@northern.com.br'
+
   layout 'mailer'
 end
